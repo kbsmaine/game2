dc57451866d06e0a6513d346116c1a7cedae225e
kbsmaine <krpelletier@hotmail.com>
2026-08-08 19:31:43 -0400

    removal

--- PLAY-TEST.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-title DEADHAUL BUILD 2.6.2 TEST SERVER
-set PORT=8806
-start "" "http://localhost:%PORT%/test-launch.html?build=2.6.2"
-py -m http.server %PORT%
--- config.js
+++
@@ -1,6 +0,0 @@
-window.DEADHAUL_CONFIG = {
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY',
-  edgeFunctionName: 'complete-raid',
-  allowDemoMode: true
-};
