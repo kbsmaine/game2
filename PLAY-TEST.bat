097d49c9b4247e49a011a51eb7442bfed9068393
kbsmaine <krpelletier@hotmail.com>
2026-08-08 22:14:54 -0400

    2.6.10

--- PLAY-TEST.bat
+++ PLAY-TEST.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
-title DEADHAUL BUILD 2.6.9 TEST SERVER
-set PORT=8812
-start "" "http://localhost:%PORT%/test-launch.html?build=2.6.9"
+title DEADHAUL BUILD 2.6.10 TEST SERVER
+set PORT=8813
+start "" "http://localhost:%PORT%/test-launch.html?build=2.6.10"
 py -m http.server %PORT%
--- START-DEMO.bat
+++ START-DEMO.bat
@@ -1,10 +1,10 @@
 @echo off
 cd /d "%~dp0"
-title DEADHAUL BUILD 2.6.9 SERVER
-set PORT=8812
+title DEADHAUL BUILD 2.6.10 SERVER
+set PORT=8813
 echo.
-echo DEADHAUL BUILD 2.6.9 - ADMIN TEST PANEL + TACTICAL INVENTORY
+echo DEADHAUL BUILD 2.6.10 - ADMIN TEST PANEL + TACTICAL INVENTORY
 echo Serving at http://localhost:%PORT%
 echo.
-start "" "http://localhost:%PORT%/index.html?build=2.6.9"
+start "" "http://localhost:%PORT%/index.html?build=2.6.10"
 py -m http.server %PORT%
