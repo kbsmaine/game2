b70ae80740521fa9de3006913c3beed2d6099803
kbsmaine <krpelletier@hotmail.com>
2026-08-09 13:46:14 -0400

    2.6.13 revert

--- START-DEMO.bat
+++ START-DEMO.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
 title DEADHAUL BUILD 2.6.13 SERVER
-set PORT=8817
+set PORT=8816
 echo.
 echo DEADHAUL BUILD 2.6.13 - PERSISTENT MAGAZINES + LOOSE AMMO
 echo Serving at http://localhost:%PORT%
