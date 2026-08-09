c659c611fddd64b1aa1006f180588e2670a7e39b
kbsmaine <krpelletier@hotmail.com>
2026-08-09 13:20:18 -0400

    2.6.14

--- PLAY-TEST.bat
+++ PLAY-TEST.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
 title DEADHAUL BUILD 2.6.13 TEST SERVER
-set PORT=8816
+set PORT=8817
 start "" "http://localhost:%PORT%/test-launch.html?build=2.6.13"
 py -m http.server %PORT%
